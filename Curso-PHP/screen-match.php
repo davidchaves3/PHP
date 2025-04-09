dac800ba4be7c4d655a6238ff09eec68209ff410
David Chaves <davidalmeidachaves3@gmail.com>
2025-04-09 10:37:00 -0300

    up

---
+++ Curso-PHP/screen-match.php
@@ -0,0 +1,10 @@
+<?php 
+
+echo "Bem-Vindo(a) ao screen match! ";
+
+$nomeFilme = "Top Gun - Maverick ";
+$anoLacamento = 2022;
+$notaFilme = 8.8;
+$incluidoNoPlano = True;
+
+echo $nomeFilme . $anoLacamento;
